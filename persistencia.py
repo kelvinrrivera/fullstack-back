6e067c519d86d8c6608055ec4a3eb7b930ca34f8
kelvinrrivera <kelvinrrivera@gmail.com>
2021-12-23 02:51:15 +0100

    Corrección de errores pylint 3

--- persistencia.py
+++ persistencia.py
@@ -1,6 +1,9 @@
+""" Persistencia """
+
 def guardar_pedido(ifname,ilname):
-
-
+    """
+    Guardar pedido
+    """
     with open("pedidos.txt", "a", encoding="utf-8") as file:
-        file.write(ifname + " " + ilname + "\n")
+        file.write("-" + ifname + " " + ilname + "\n")
         file.close()
